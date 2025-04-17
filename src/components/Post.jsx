b9f83f542f63622d4c0991875c893427993f79a9
Juma <gerald.juma@student.moringaschool.com>
2025-04-17 11:27:41 +0300

    added singlepost component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Post = ({post}) => {
+
   return (
-    <div>
-        <h6>{post.title}</h6>
-        <p>{post.body}</p>
+    <div className='single-post'>
+        <h6>{post.title.substring(0, 10)}</h6>
+        <p>{post.body.substring(0, 20)}</p>
+        <button><Link to={`/posts/${post.id}`}>Read more..</Link></button>
     </div>
   )
 }
